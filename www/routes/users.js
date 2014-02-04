17b22443732fd1a383700acf289342a57dd4de5d
Rudi Starcevic <ooly.me@gmail.com>
2014-02-04 11:21:22 +0800

    Update to hashbang is user routes

--- www/routes/users.js
+++ www/routes/users.js
@@ -11,7 +11,7 @@ module.exports = function (config, app, passport) {
 
   // Local strategy route
   app.post('/users/session',
-    passport.authenticate('local', {failureRedirect: '/#/signin?msg=error'}),
+    passport.authenticate('local', {failureRedirect: '/#!/signin?msg=error'}),
     users.session
   );
 
@@ -19,7 +19,7 @@ module.exports = function (config, app, passport) {
   app.get('/auth/github', passport.authenticate('github'));
 
   app.get('/auth/github/callback',
-    passport.authenticate('github', {failureRedirect: '/#/signin?msg=error'}), users.authCallback);
+    passport.authenticate('github', {failureRedirect: '/#!/signin?msg=error'}), users.authCallback);
 
   // Google strategy oauth routes
   app.get('/auth/google',
@@ -31,12 +31,12 @@ module.exports = function (config, app, passport) {
     }));
 
   app.get('/auth/google/callback',
-    passport.authenticate('google', {failureRedirect: '/#/signin?msg=error'}), users.authCallback);
+    passport.authenticate('google', {failureRedirect: '/#!/signin?msg=error'}), users.authCallback);
 
   // Facebook strategy oauth routes
   app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email', 'user_about_me']}));
 
   app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', {failureRedirect: '/#/signin?msg=error'}), users.authCallback);
+    passport.authenticate('facebook', {failureRedirect: '/#!/signin?msg=error'}), users.authCallback);
 
 };
